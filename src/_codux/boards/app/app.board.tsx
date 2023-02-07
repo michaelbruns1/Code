6e3dafadbac0ba501d24b1ac37b8395cae06bbe3
Michael Bruns <michael@edhost.io>
2023-02-07 04:34:41 -0800

    moved project

---
+++ src/_codux/boards/app/app.board.tsx
@@ -0,0 +1,11 @@
+import { createBoard } from '@wixc3/react-board';
+import App from '../../../App';
+
+export default createBoard({
+    name: 'App',
+    Board: () => <App />,
+    environmentProps: {
+        windowBackgroundColor: '#ef9a9a',
+        canvasBackgroundColor: '#364c69',
+    },
+});
